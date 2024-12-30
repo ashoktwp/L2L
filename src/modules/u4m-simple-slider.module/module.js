c1a93a012f3ea30f469e15e271b66f9475ed352e
pinkiwebplant <pinki@thewebplant.in>
2024-12-30 18:51:03 +0530

    bugherd issues update

--- src/modules/u4m-simple-slider.module/module.js
+++ src/modules/u4m-simple-slider.module/module.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(' .u4m-simple-slider .Cmslider').slick({
     dots: true,
-    speed: 300,
+    speed: 500,
     arrows: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,        
-    autoplaySpeed: 3000,  
+    autoplay: true,
+    autoplaySpeed: 6000,
     pauseOnFocus: false,
-    pauseOnHover: true,   
+    pauseOnHover: true,
   });
 });
